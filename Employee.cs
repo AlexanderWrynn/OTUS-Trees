a84c9c2800281625ac56b6dfd740f12407543c6d
Alex Z <can4you@yandex.ru>
2021-10-17 17:47:00 +0300

    Добавьте файлы проекта.

---
+++ Employee.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTUS_Trees
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public int Salary { get; set; }
+
+        public Employee Left { get; set; }
+        public Employee Right { get; set; }
+
+        public static void Add(Employee current, Employee toAdd)
+        {
+            if (toAdd.Salary < current.Salary)
+            {
+                if (current.Left != null)
+                {
+                    Add(current.Left, toAdd);
+                }
+
+                else
+                {
+                    current.Left = toAdd;
+                }
+            }
+
+            else
+            {
+                if (current.Right != null)
+                {
+                    Add(current.Right, toAdd);
+                }
+
+                else
+                {
+                    current.Right = toAdd;
+                }
+            }
+        }
+
+        public static void Traverse(Employee employee)
+        {
+            if (employee.Left != null)
+            {
+                Traverse(employee.Left);
+            }
+
+            Console.WriteLine(employee);
+
+            if (employee.Right != null)
+            {
+                Traverse(employee.Right);
+            }
+
+        }
+
+        public static Employee Search(Employee current, int value)
+        {
+            if (value < current.Salary)
+            {
+                if (current.Left != null)
+                {
+                    return Search(current.Left, value);
+                }
+
+                else
+                {
+                    return null;
+                }
+            }
+
+            else if (value > current.Salary)
+            {
+                if (current.Right != null)
+                {
+                    return Search(current.Right, value);
+                }
+
+                else
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                return current;
+            }
+        }
+
+        public override string ToString()
+        {
+            return Name + " - " + Salary.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace OTUS_Trees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя и зарплату. Для окончания ввода введите пустое имя");
+            Console.WriteLine();
+
+            Employee employee = null;
+            int flag = 0;
+
+            while (flag == 0)
+            {
+                while (true)
+                {
+                    string employeeName = Console.ReadLine();
+
+                    if (string.IsNullOrEmpty(employeeName))
+                    {
+                        break;
+                    }
+
+                    int employeeSalary = CheckedInput();
+
+                    if (employee == null)
+                    {
+                        employee = new Employee { Name = employeeName, Salary = employeeSalary };
+                    }
+
+                    else
+                    {
+                        Employee.Add(employee, new Employee { Name = employeeName, Salary = employeeSalary });
+                    }
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Имя - сотрудник");
+                Employee.Traverse(employee);
+
+                while (true)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Интересующий размер зарплаты");
+                    Console.WriteLine();
+
+                    int toSearch = CheckedInput();
+
+                    Employee foundEmployee = Employee.Search(employee, toSearch);
+
+                    if (foundEmployee == null)
+                    {
+                        Console.WriteLine("Такой сотрудник не найден");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine(foundEmployee);
+                    }
+
+                    Console.WriteLine();
+                    Console.WriteLine("Введите:\n" +
+                                        "0 - для перехода в начало программы;\n" +
+                                        "1 - для продолжения поиска по зарплате;\n" +
+                                        "2 - для выхода.");
+                    Console.WriteLine();
+
+                    int inputNum = CheckedInput();
+
+                    if (inputNum == 0)
+                    {
+                        employee = null;
+                        break;
+                    }
+
+                    if (inputNum == 1)
+                    {
+                        continue;
+                    }
+
+                    if (inputNum == 2)
+                    {
+                        flag = 1;
+                        break;
+                    }
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+
+        static int CheckedInput()
+        {
+            while (true)
+            {
+                bool inputNum = int.TryParse(Console.ReadLine(), out int outputNum);
+
+                if (inputNum)
+                {
+                    return outputNum;
+                }
+
+                Console.WriteLine("Некорректный ввод. Повторите.");
+                Console.WriteLine();
+            }
+        }
+    }
+}
